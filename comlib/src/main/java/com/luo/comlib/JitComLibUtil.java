8e9085fd25cb7d594ad82758c7733b460cd5e012
luozhimin0918 <838111691@qq.com>
2016-06-02 14:38:39 +0800

    only  one lib  comlib

---
+++ comlib/src/main/java/com/luo/comlib/JitComLibUtil.java
@@ -0,0 +1,12 @@
+package com.luo.comlib;
+
+/**
+ * Created by Administrator on 2016/5/31.
+ */
+public class JitComLibUtil {
+
+    public  static  boolean isJitpack(){
+
+        return true;
+    }
+}
