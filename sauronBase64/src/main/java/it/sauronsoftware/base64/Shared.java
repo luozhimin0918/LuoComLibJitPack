a693ada8b1da6f4809166283ec34454e22b2351b
Android-罗志敏 <54543534@domain.com>
2017-08-16 14:45:04 +0800

    sauronBase64  add

---
+++ sauronBase64/src/main/java/it/sauronsoftware/base64/Shared.java
@@ -0,0 +1,14 @@
+package it.sauronsoftware.base64;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+
+
+class Shared {
+    static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    static char pad = 61;
+
+    Shared() {
+    }
+}
